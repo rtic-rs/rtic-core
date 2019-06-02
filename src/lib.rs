22006d11dbd9b4997954ad57dcb02e1914c70e2c
Jorge Aparicio <jorge@japaric.io>
2019-06-02 12:33:33 +0200

    Mutex and Exclusive

--- src/lib.rs
+++ src/lib.rs
@@ -1,81 +1,70 @@
 //! Core abstractions of the Real Time For the Masses (RTFM) framework
 //!
-//! You can write generic *libraries* by using the `Resource` trait in this
-//! crate. If you want to write application code then you'll need an
-//! implementation of the RTFM framework for a particular architecture.
-//! Currently there are implementations for these two architectures:
+//! You can write generic *libraries* using the `Mutex` trait in this crate. If you want to write
+//! application code then you'll need an *implementation* of the RTFM framework for a particular
+//! architecture. Currently, there are implementations for these architectures and OSes:
 //!
 //! - [ARM Cortex-M](https://crates.io/crates/cortex-m-rtfm)
-//! - [MSP430](https://crates.io/crates/msp430-rtfm)
+// - [Linux]
+// - [MSP430]
+// - [RISC-V]
+
 #![deny(missing_docs)]
+#![deny(rust_2018_compatibility)]
+#![deny(rust_2018_idioms)]
 #![deny(warnings)]
 #![no_std]
 
-use core::u8;
-use core::marker::PhantomData;
+use core::ops;
 
-/// A resource, a mechanism to share data between tasks
-pub unsafe trait Resource {
-    /// The data protected by the resource
-    type Data: Send;
-
-    /// Borrows the resource data for the duration of a critical section
-    ///
-    /// # Panics
-    ///
-    /// This will `panic!` if the threshold is not high enough to protect the
-    /// resource data from data races
-    fn borrow<'cs>(&'cs self, t: &'cs Threshold) -> &'cs Self::Data;
-
-    /// Mutable variant of `borrow`
-    fn borrow_mut<'cs>(&'cs mut self, t: &'cs Threshold) -> &'cs mut Self::Data;
-
-    /// Claims the resource data for the span of the closure `f`. For the
-    /// duration of the closure other tasks that may access the resource data
-    /// are prevented from preempting the current task.
-    fn claim<R, F>(&self, t: &mut Threshold, f: F) -> R
-    where
-        F: FnOnce(&Self::Data, &mut Threshold) -> R;
-
-    /// Mutable variant of `claim`
-    fn claim_mut<R, F>(&mut self, t: &mut Threshold, f: F) -> R
-    where
-        F: FnOnce(&mut Self::Data, &mut Threshold) -> R;
-}
-
-/// Preemption threshold token
+/// Memory safe access to shared resources
 ///
-/// The preemption threshold indicates the priority a task must have to preempt
-/// the current context. For example a threshold of 2 indicates that only
-/// interrupts / exceptions with a priority of 3 or greater can preempt the
-/// current context
-pub struct Threshold {
-    value: u8,
-    _not_send: PhantomData<*const ()>,
+/// In RTFM, locks are implemented as critical sections that prevent other tasks from *starting*.
+/// These critical sections are implemented by temporarily increasing the dynamic priority of the
+/// current context. Entering and leaving these critical sections is always done in bounded constant
+/// time (a few instructions in bare metal contexts).
+pub trait Mutex {
+    /// Data protected by the mutex
+    type T;
+
+    /// Creates a critical section and grants temporary access to the protected data
+    fn lock<R>(&mut self, f: impl FnOnce(&mut Self::T) -> R) -> R;
 }
 
-impl Threshold {
-    /// Creates a new `Threshold` token
-    ///
-    /// This API is meant to be used to create abstractions and not to be
-    /// directly used by applications.
-    pub unsafe fn new(value: u8) -> Self {
-        Threshold {
-            value,
-            _not_send: PhantomData,
-        }
-    }
+impl<'a, M> Mutex for &'a mut M
+where
+    M: Mutex,
+{
+    type T = M::T;
 
-    /// Creates a `Threshold` token with maximum value
-    ///
-    /// This API is meant to be used to create abstractions and not to be
-    /// directly used by applications.
-    pub unsafe fn max() -> Self {
-        Self::new(u8::MAX)
+    fn lock<R>(&mut self, f: impl FnOnce(&mut M::T) -> R) -> R {
+        M::lock(self, f)
     }
+}
 
-    /// Returns the value of this `Threshold` token
-    pub fn value(&self) -> u8 {
-        self.value
+/// Newtype over `&'a mut T` that implements the `Mutex` trait
+///
+/// The `Mutex` implementation for this type is a no-op: no critical section is created
+pub struct Exclusive<'a, T>(pub &'a mut T);
+
+impl<'a, T> Mutex for Exclusive<'a, T> {
+    type T = T;
+
+    fn lock<R>(&mut self, f: impl FnOnce(&mut T) -> R) -> R {
+        f(self.0)
+    }
+}
+
+impl<'a, T> ops::Deref for Exclusive<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.0
+    }
+}
+
+impl<'a, T> ops::DerefMut for Exclusive<'a, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        self.0
     }
 }
