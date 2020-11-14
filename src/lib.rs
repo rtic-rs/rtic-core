6c68da2daf659ab04c88fa7a622e2e0a2e88bdca
Henrik Tjäder <henrik@tjaders.com>
2020-11-14 15:22:53 +0000

    Make cargofmt happy

--- src/lib.rs
+++ src/lib.rs
@@ -172,4 +172,3 @@ pub mod prelude {
     make_tuple_impl!(TupleExt11, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11);
     make_tuple_impl!(TupleExt12, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12);
 }
-
